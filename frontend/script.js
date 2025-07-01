96a2e2efdb497d12dca80b4075372bcec0fbe9b0
Cyan Numerical <nurfashansyamil@gmail.com>
2025-07-02 00:40:09 +0800

    update and fix folder and code

--- frontend/script.js
+++ frontend/script.js
@@ -8,7 +8,7 @@
                 category: document.getElementById('category').value,
                 description: document.getElementById('description').value
             };
-            const response = await fetch('/api/submit_ticket', {    
+            const response = await fetch('https://quickaid-api-grp3.azurewebsites.net/api/submit_ticket', {    
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(ticket)
